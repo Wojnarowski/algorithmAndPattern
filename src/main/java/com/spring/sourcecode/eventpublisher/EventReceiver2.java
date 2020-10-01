567fe255f0029a452ca65bfd821c5da45e4ae149
guozhizhong <guozhizhong@czb365.com>
2020-10-01 23:49:50 +0800

    增加事件监听机制代码

--- src/main/java/com/spring/sourcecode/eventpublisher/EventReceiver2.java
+++ src/main/java/com/spring/sourcecode/eventpublisher/EventReceiver2.java
@@ -5,9 +5,10 @@ import org.springframework.context.ApplicationListener;
 /**
  * 3.最后编写 事件的接收者EventReceiver
  */
-public class EventReceiver implements ApplicationListener<TradeEvent>{
+public class EventReceiver2 implements ApplicationListener<TradeEvent>{
 
     public void onApplicationEvent(TradeEvent tradeEvent) {
-        System.out.println("监听到的事件1："+tradeEvent.getSource());
+        System.out.println("监听到的事件2："+tradeEvent.getSource());
+        System.out.println("我又干了点别的事儿");
     }
 }
