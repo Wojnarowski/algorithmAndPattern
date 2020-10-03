ca0cc469621199f83a6ec6f5b91979bfce4c7901
guozhizhong <guo_zhizhong@sina.com>
2020-10-03 15:14:51 +0800

    模板方法,新增代码
    Signed-off-by: guozhizhong <guo_zhizhong@sina.com>

---
+++ src/main/java/pattern/template/HummerH1Model.java
@@ -0,0 +1,23 @@
+package pattern.template;
+
+public  class HummerH1Model extends HummerModel{
+
+
+    public void start() {
+        System.out.println("悍马H1 start");
+    }
+
+    public void stop() {
+        System.out.println("悍马H1 stop");
+    }
+
+    public void alarm() {
+        System.out.println("悍马H1 alarm");
+    }
+
+    public void engineBoom() {
+        System.out.println("悍马H1 engineBoom");
+    }
+
+
+}
---
+++ src/main/java/pattern/template/HummerH2Model.java
@@ -0,0 +1,22 @@
+package pattern.template;
+
+public  class HummerH2Model extends HummerModel{
+
+
+    public void start() {
+        System.out.println("悍马H2 start");
+    }
+
+    public void stop() {
+        System.out.println("悍马H2 stop");
+    }
+
+    public void alarm() {
+        System.out.println("悍马H2 alarm");
+    }
+
+    public void engineBoom() {
+        System.out.println("悍马H2 engineBoom");
+    }
+
+}
