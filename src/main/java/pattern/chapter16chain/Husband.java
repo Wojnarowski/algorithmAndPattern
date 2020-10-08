865ad5181c0decb20cb8b8352728c8c0f0c6f27f
guozhizhong <guo_zhizhong@sina.com>
2020-10-08 23:20:52 +0800

    责任链,新增代码
    Signed-off-by: guozhizhong <guo_zhizhong@sina.com>

---
+++ src/main/java/pattern/chapter16chain/Husband.java
@@ -0,0 +1,15 @@
+package pattern.chapter16chain;
+
+public class Husband extends Handler{
+
+
+    public Husband() {
+        super(Handler.HUSBAND_LEVEL_REQUEST);
+    }
+
+    protected void response(IWomen women) {
+        System.out.println("--------妻子向丈夫请示-------");
+        System.out.println(women.getRequest());
+        System.out.println("丈夫的答复是：同意\n");
+    }
+}
