8b4fe28d0dfaa6721876090d7d6408dd0e8a1ff5
guozhizhong <guo_zhizhong@sina.com>
2020-10-13 17:59:41 +0800

    组合模式

---
+++ src/main/java/pattern/chapter21Composite/Client.java
@@ -0,0 +1,12 @@
+package pattern.chapter21Composite;
+
+/**
+ * 组合模式(Composite Pattern)也叫合成模式，有时又叫做部分-整体模式（Part-Whole），
+ * 主要是用来描述部分与整体的关系，其定义如下：
+ */
+public class Client {
+
+    public static void main(String[] args) {
+
+    }
+}
