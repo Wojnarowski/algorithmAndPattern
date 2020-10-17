247449324578b3ec6244688cdbad19f26f2c0993
guozhizhong <guo_zhizhong@sina.com>
2020-10-17 11:27:41 +0800

    新增代码
    Signed-off-by: guozhizhong <guo_zhizhong@sina.com>

---
+++ src/main/java/pattern/chapter25Visitor/Client.java
@@ -0,0 +1,11 @@
+package pattern.chapter25Visitor;
+
+/**
+ * 访问者模式（Visitor Pattern）是一个相对简单的模式，
+ * 其定义如下：Represent an operation to be performed on the elements of an object structure.
+ * Visitor lets you define a new operation without changing the classes of
+ * the elements on which it operates. （封装一些作用于某种 数据结构中的各元素的
+ * 操作，它可以在不改变数据结构的前提下定义作用于这些元素的新的 操作。）
+ */
+public class Client {
+}
