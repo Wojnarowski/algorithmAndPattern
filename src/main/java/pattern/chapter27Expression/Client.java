584cbe87cfe7e38bf677c6f2a5c33d909723623f
guozhizhong <guo_zhizhong@sina.com>
2020-10-17 17:16:46 +0800

    享元
    Signed-off-by: guozhizhong <guo_zhizhong@sina.com>

--- src/main/java/pattern/chapter27Expression/Client.java
+++ src/main/java/pattern/chapter27Expression/Client.java
@@ -10,9 +10,9 @@ public class Client {
          for(;;){
         // 进行语法判断，并产生递归调用
          }
-        // 产生一个完整的语法树，由各个具体的语法分析进行解析
-         Expression exp = stack.pop();
-        // 具体元素进入场景
-         exp.interpreter(ctx);
+//        // 产生一个完整的语法树，由各个具体的语法分析进行解析
+//         Expression exp = stack.pop();
+//        // 具体元素进入场景
+//         exp.interpreter(ctx);
          }
 }
