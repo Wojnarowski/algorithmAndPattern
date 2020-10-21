e52dfe4b8bc67049f9f1478c17be299ebafc1734
guozhizhong <guo_zhizhong@sina.com>
2020-10-21 23:18:12 +0800

    提交
    Signed-off-by: guozhizhong <guo_zhizhong@sina.com>

--- src/main/java/zuo/guide/chapter22DeleteReverseKNode/Client.java
+++ src/main/java/zuo/guide/chapter22DeleteReverseKNode/Client.java
@@ -2,6 +2,10 @@ package zuo.guide.chapter22DeleteReverseKNode;
 
 /**
  * 在单链表或者双向链表中删除倒数第K个节点
+ * 分别实现两个函数，一个可以删除单向链表中的倒数第K个节点。
+ * 另一个可以删除双向链表中倒数第K个节点
+ *
+ * 如果链表长度为N，时间复杂度达到O（N） ，额外空间复杂度度到O（1）
  *
  */
 public class Client {
