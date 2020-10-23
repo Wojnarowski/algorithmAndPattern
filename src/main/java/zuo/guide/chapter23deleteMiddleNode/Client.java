6c5330d1af9239e84acc58bc0c1b68135168d9c0
guozhizhong <guo_zhizhong@sina.com>
2020-10-23 18:16:26 +0800

    增加

--- src/main/java/zuo/guide/chapter23deleteMiddleNode/Client.java
+++ src/main/java/zuo/guide/chapter23deleteMiddleNode/Client.java
@@ -39,4 +39,32 @@ public class Client {
     }
 
 
+    public Node removeByRatio(Node head,int a, int b){
+
+        if(a<1||a>b){
+            return head;
+        }
+        int n=0;
+        Node cur =head;
+        while(cur!=null){
+            n++;
+            cur=cur.next;
+        }
+        n=(int)Math.ceil(((double)(n*a))/(double)b);
+        if(n==1){
+            head=head.next;
+        }
+
+        if(n>1){
+            cur=head;
+            while(--n!=1){
+                cur=cur.next;
+            }
+            cur.next=cur.next.next;
+        }
+
+        return head;
+    }
+
+
 }
