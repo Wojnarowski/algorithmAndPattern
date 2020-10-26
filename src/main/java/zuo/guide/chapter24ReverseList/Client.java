14d6ea65cc7939d529a9cc9fd4349777d6657e21
guozhizhong <guo_zhizhong@sina.com>
2020-10-26 17:57:15 +0800

    修改单向链表

--- src/main/java/zuo/guide/chapter24ReverseList/Client.java
+++ src/main/java/zuo/guide/chapter24ReverseList/Client.java
@@ -6,14 +6,32 @@ package zuo.guide.chapter24ReverseList;
  */
 public class Client {
 
-    public Node reverseList(Node head){
+    public static void main(String[] args) {
+        Node nodePart1 = new Node(1);
+        Node nodePart2 = new Node(2);
+        Node nodePart3 = new Node(3);
+        Node nodePart4 = new Node(4);
+        Node nodePart5 = new Node(5);
+        nodePart1.setNext(nodePart2);
+        nodePart2.setNext(nodePart3);
+        nodePart3.setNext(nodePart4);
+        nodePart4.setNext(nodePart5);
+        System.out.println(nodePart1);
+        System.out.println("------------------");
+        Node node = reverseList(nodePart1);
+        System.out.println(node);
+    }
+
+    public static Node reverseList(Node head){
         Node pre = null;
         Node next= null;
-        while(head!=null){
-            next=head.next;
-            head.next=pre;
-            pre=head;
-            head=next;
+        Node cur = head;
+        while(cur!=null){
+            next=cur.next;
+            cur.next=pre;
+            pre=cur;
+            cur=next;
+
         }
         return pre;
     }
