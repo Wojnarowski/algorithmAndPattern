491ecf2754c77a2f18f038d613aab1c17bd75673
guozhizhong <guo_zhizhong@sina.com>
2020-11-17 15:50:30 +0800

    增加双向链表反转接口

--- src/main/java/zuo/vip/class03/Code01_ReverseList.java
+++ src/main/java/zuo/vip/class03/Code01_ReverseList.java
@@ -62,4 +62,17 @@ public class Code01_ReverseList {
         return pre;
     }
 
+    public static DoubleNode reverseDoubleNode(DoubleNode head){
+        DoubleNode pre = null;
+        DoubleNode next = null;
+        while(head!=null){
+            next=head.next;
+            head.next=pre;
+            head.last=next;
+            pre=head;
+            head=next;
+        }
+        return pre;
+    }
+
 }
