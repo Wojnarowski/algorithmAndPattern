35d97bdd380100d381de6761c1712f15680a03be
guozhizhong <guo_zhizhong@sina.com>
2020-11-17 10:15:15 +0800

    临时提交

---
+++ src/main/java/zuo/vip/class03/Code01_ReverseList.java
@@ -0,0 +1,65 @@
+package zuo.vip.class03;
+
+public class Code01_ReverseList {
+
+
+    public static void main(String[] args) {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+//        Node node3 = new Node(3);
+//        Node node4 = new Node(4);
+//        Node node5 = new Node(5);
+        node1.next=node2;
+//        node2.next=node3;
+//        node3.next=node4;
+//        node4.next=node5;
+
+        Node node = reverseLinkedList(node1);
+        System.out.println(node.value);
+
+    }
+
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int data) {
+            value = data;
+        }
+    }
+
+    public static class DoubleNode {
+        public int value;
+        public DoubleNode last;
+        public DoubleNode next;
+
+        public DoubleNode(int data) {
+            value = data;
+        }
+    }
+
+    //  head
+    //   a    ->   b    ->  c  ->  null
+    //   c    ->   b    ->  a  ->  null
+
+    /**
+     * 过程 第一次后
+     * b    ->  a     ->  c  ->  null
+     * 第一次后
+     * c    ->  b    ->  a  ->  null
+     * @param head
+     * @return
+     */
+    public static Node reverseLinkedList(Node head) {
+        Node pre = null;
+        Node next = null;
+        while (head != null) {
+            next = head.next;  //next =b
+            head.next = pre;   // 将头的下一个指针指向前一个pre
+            pre = head;        //将头给pre
+            head = next;       //将下一个给头结点
+        }
+        return pre;
+    }
+
+}
