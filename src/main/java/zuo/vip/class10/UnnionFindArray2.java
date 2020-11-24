783eaec5345d9eab3b1967d752275420be04319e
郭智忠 <guo_zhizhong@sina.com>
2020-11-24 10:03:06 +0800

    修改

---
+++ src/main/java/zuo/vip/class10/UnnionFindArray2.java
@@ -0,0 +1,86 @@
+package zuo.vip.class10;
+
+/**
+ * 用数组实现并查集，元素内数字代表集合号
+ */
+public class UnnionFindArray2 {
+
+    private int[] parent;
+    private int size;
+
+    public UnnionFindArray2(int size) {
+        this.size = size;
+        parent = new int[size];
+        for (int i = 0; i < size; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public int find(int element) {
+        while (element != parent[element]) {
+            element = parent[element];
+        }
+        return element;
+    }
+
+    public boolean isConnected(int firstElement, int secondElement) {
+        return find(firstElement) == find(secondElement);
+    }
+
+    public void unionElements(int firstElement, int secondElement) {
+        int firstRoot = find(firstElement);
+        int secondRoot = find(secondElement);
+        if (firstRoot == secondRoot) {
+            return;
+        }
+        parent[firstRoot] = secondRoot;
+    }
+
+    /**
+     * 本并查集使用数组实现，为了更直观地看清内部数据，采用打印数组
+     */
+    private void printArr() {
+        for (int parent : this.parent) {
+            System.out.print(parent + "\t");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        UnnionFindArray2 union = new UnnionFindArray2(n);
+        System.out.println("初始：");
+        union.printArr();
+
+        System.out.println("连接了5 6");
+        union.unionElements(5, 6);
+        union.printArr();
+
+        System.out.println("连接了1 2");
+        union.unionElements(1, 2);
+        union.printArr();
+
+        System.out.println("连接了2 3");
+        union.unionElements(2, 3);
+        union.printArr();
+
+        System.out.println("连接了1 4");
+        union.unionElements(1, 4);
+        union.printArr();
+
+        System.out.println("连接了1 5");
+        union.unionElements(1, 5);
+        union.printArr();
+
+        System.out.println("1  6 是否连接：" + union.isConnected(1, 6));
+
+        System.out.println("1  8 是否连接：" + union.isConnected(1, 8));
+    }
+
+
+
+
+
+
+
+}
