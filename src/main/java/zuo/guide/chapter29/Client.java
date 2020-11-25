70d5309ee32dd0a3e0ec36ae63b77423f0363a7f
guozhizhong <guo_zhizhong@sina.com>
2020-11-25 09:02:39 +0800

    相加链表

--- src/main/java/zuo/guide/chapter29/Client.java
+++ src/main/java/zuo/guide/chapter29/Client.java
@@ -1,5 +1,7 @@
 package zuo.guide.chapter29;
 
+import java.util.Stack;
+
 /**
  * 两个单链表生成相加链表
  * 假设链表中每一个节点的值都在0～9 之间，那么链表整体就可以代表一个整数。
@@ -8,4 +10,29 @@ package zuo.guide.chapter29;
  * 例如∶链表1为 9->3->7，链表2为6->3，最后生成新的结果链表为1->0->0->0。
  */
 public class Client {
+
+    public class Node{
+        private int value;
+        private Node next;
+        public Node(int data ){
+            this.value=data;
+        }
+    }
+
+    public Node addList(Node head1,Node head2){
+        Stack<Integer> s1 = new Stack<Integer>();
+        Stack<Integer> s2 = new Stack<Integer>();
+        while(head1!=null){
+            s1.push(head1.value);
+            head1=head1.next;
+        }
+
+        while(head2!=null){
+            s2.push(head2.value);
+            head2=head2.next;
+        }
+
+
+
+    }
 }
