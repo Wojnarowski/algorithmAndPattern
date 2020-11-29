e250183999859e4f95581ef083a91812305a42c1
guozhizhong <guo_zhizhong@sina.com>
2020-11-29 23:08:30 +0800

    增加方法2

--- src/main/java/zuo/guide/chapter29/Client.java
+++ src/main/java/zuo/guide/chapter29/Client.java
@@ -98,7 +98,34 @@ public class Client {
 
         head1=reverseList(head1);
         head2=reverseList(head2);
-        return null;
+
+        int ca=0;
+        int n1=0;
+        int n2=0;
+        int n=0;
+        Node c1=head1;
+        Node c2=head2;
+        Node node =null;
+        Node pre =null;
+        while(c1!=null || c2!=null){
+            n1= c1 !=null?c1.value:0;
+            n2= c2 !=null?c2.value:0;
+            n=n1 + n2 + ca;
+            pre=node;
+            node =new Node(n % 10 );
+            node.next=pre;
+            ca=n / 10;
+            c1  = c1 !=null ? c1.next:null;
+            c2 = c2 !=null ? c2.next:null;
+        }
+        if(ca == 1){
+            pre=node;
+            node = new Node(1);
+            node.next=pre;
+        }
+        reverseList(head1);
+        reverseList(head2);
+        return node;
     }
 
     private static Node reverseList(Node head) {
