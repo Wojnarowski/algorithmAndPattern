969df5d25b294e586f1d2bc3530314b4f03b35f6
guozhizhong <guo_zhizhong@sina.com>
2020-12-02 21:43:54 +0800

    新增

--- src/main/java/zuo/guide/chapter210/Client.java
+++ src/main/java/zuo/guide/chapter210/Client.java
@@ -47,4 +47,38 @@ public class Client {
         return n1;
     }
 
+    public Node  noLoop(Node head1 ,Node head2){
+        if(head1 ==null || head2==null){
+            return null;
+        }
+        Node cur1 =head1;
+        Node cur2=head2;
+        int n=0;
+        while(cur1.next!=null){
+            n++;
+            cur1=cur1.next;
+        }
+        while (cur2.next!=null){
+            n--;
+            cur2=cur2.next;
+        }
+        if(cur1 != cur2){
+            return  null;
+        }
+        cur1=n>0?head1:head2;
+        cur2= cur1 ==head1?head2:head1;
+        n=Math.abs(n);
+        while (n!=0){
+            n--;
+            cur1=cur1.next;
+        }
+        while(cur1!=cur2){
+            cur1=cur1.next;
+            cur2=cur2.next;
+        }
+        return cur1;
+    }
+
+
+
 }
