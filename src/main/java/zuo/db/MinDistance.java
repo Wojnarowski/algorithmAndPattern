36b0b5e0f7ada5554b780d8c9a7ce85c10a3bfb6
guozhizhong <guo_zhizhong@sina.com>
2020-12-17 19:11:59 +0800

    修改

---
+++ src/main/java/zuo/db/MinDistance.java
@@ -0,0 +1,19 @@
+package zuo.db;
+
+/**
+ * 编辑距离
+ * 给你两个单词 word1 和 word2 ，请你计算出word1 转成 word2 所需要的最少操作数
+ * 你可以对一个单词做如下操作
+ * 1.插入一个字符
+ * 2.删除一个字符
+ * 3.替换一个字符
+ * 实例 如 word1='horse' ，word2='ros'
+ *  解释
+ *      horse->rorse (将h替换成r)
+ *      rorse->rose （删除r）
+ *      rose->ros (删除e)
+ *
+ *
+ */
+public class MinDistance {
+}
