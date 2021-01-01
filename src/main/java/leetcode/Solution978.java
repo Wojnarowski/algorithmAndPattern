37c3da2ae2c36584a07c1aa4ace445149a86522f
guozhizhong <guo_zhizhong@sina.com>
2021-01-01 16:57:16 +0800

    提交代码

--- src/main/java/leetcode/Solution978.java
+++ src/main/java/leetcode/Solution978.java
@@ -12,16 +12,17 @@ package leetcode;
   
 
  示例 1：
-
  输入：[9,4,2,10,7,8,8,1,9]
  输出：5
  解释：(A[1] > A[2] < A[3] > A[4] < A[5])
- 示例 2：
 
+
+ 示例 2：
  输入：[4,8,12,16]
  输出：2
- 示例 3：
 
+
+ 示例 3：
  输入：[100]
  输出：1
   
@@ -38,22 +39,41 @@ package leetcode;
 public class Solution978 {
 
     public static void main(String[] args) {
-        
+        //int [] arr = new int[] {9,4,2,10,7,8,8,1,9};
+        //int [] arr = new int[] {4,8,12,16};
+        int [] arr = new int[] {100};
+        System.out.println(maxTurbulenceSize(arr));
+
     }
 
     public static int maxTurbulenceSize(int [] arr){
         if(arr==null || arr.length==0){
             return 0;
         }
-        //当长度为 i 时 湍流子数组长度
         int n = arr.length;
-        int [] dp = new int [n+1];
-        for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-
-
-            }
+        if(arr.length<=1){
+            return n;
+        }
+        if(arr.length==2){
+            return arr[1] == arr[0]?1:2;
         }
 
+        //动态规划数组
+        int [] dp = new int [n];
+        //赋初值
+        dp[0]=1;
+        dp[1]=arr[1] == arr[0]?1:2;
+
+        int ans =0;
+        for(int i=2;i<n;i++){
+            //dp的初始状态与 arr[i] 和 arr[i-1] 是否相关
+            dp[i]=arr[i] ==arr[i-1]?1:2;
+            //判断湍流的条件 arr[i] 和 arr[i-1] 和 arr[i-2] 相关
+          if((arr[i-1]>arr[i] && arr[i-1]>arr[i-2]) || (arr[i-1]<arr[i] && arr[i-1]<arr[i-2])){
+              dp[i]=dp[i-1]+1;
+          }
+          ans = Math.max(ans,dp[i]);
+        }
+        return ans;
     }
 }
