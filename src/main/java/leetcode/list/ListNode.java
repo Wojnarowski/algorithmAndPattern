306ec5767b1d146d1fa1496c7f3cd1e7a65cfff2
guozhizhong <guo_zhizhong@sina.com>
2021-02-19 10:59:02 +0800

    提交

--- src/main/java/leetcode/list/ListNode.java
+++ src/main/java/leetcode/list/ListNode.java
@@ -15,4 +15,21 @@ public class ListNode {
                     ", next=" + next +
                     '}';
       }
+
+      /**
+       * 返回长度为5 值为1到5递增的链表头结点
+       * @return
+       */
+      public static ListNode getListHeader(){
+            ListNode listNode11 = new ListNode(1);
+            ListNode listNode12 = new ListNode(2);
+            ListNode listNode13 = new ListNode(3);
+            ListNode listNode14 = new ListNode(4);
+            ListNode listNode15 = new ListNode(5);
+            listNode11.next=listNode12;
+            listNode12.next=listNode13;
+            listNode13.next=listNode14;
+            listNode14.next=listNode15;
+            return listNode11;
+      }
 }
---
+++ src/main/java/leetcode/list/Solution61.java
@@ -0,0 +1,57 @@
+package leetcode.list;
+
+/**
+ *给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数。
+ *
+ * 示例 1:
+ *
+ * 输入: 1->2->3->4->5->NULL, k = 2
+ * 输出: 4->5->1->2->3->NULL
+ * 解释:
+ * 向右旋转 1 步: 5->1->2->3->4->NULL
+ * 向右旋转 2 步: 4->5->1->2->3->NULL
+ * 示例 2:
+ *
+ * 输入: 0->1->2->NULL, k = 4
+ * 输出: 2->0->1->NULL
+ * 解释:
+ * 向右旋转 1 步: 2->0->1->NULL
+ * 向右旋转 2 步: 1->2->0->NULL
+ * 向右旋转 3 步: 0->1->2->NULL
+ * 向右旋转 4 步: 2->0->1->NULL
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/rotate-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+
+
+public class Solution61 {
+
+
+
+    public static ListNode rotateRight(ListNode head, int k) {
+        if(head==null || k<1){
+            return head;
+        }
+        ListNode pre = head;
+        ListNode last=head;
+        while (head.next.next !=null){
+            head=head.next;
+        }
+        pre=head;
+        last=head.next;
+        head.next=null;
+        last.next=pre;
+        return last;
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println(rotateRight(ListNode.getListHeader(),2));
+
+
+    }
+}
