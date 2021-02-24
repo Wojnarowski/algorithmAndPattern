e3e9fff1216cf5904a20b8e7046149d9496535ba
guozhizhong <guo_zhizhong@sina.com>
2021-02-24 23:38:08 +0800

    提交

--- src/main/java/leetcode/everyday/AlgorithmUtil.java
+++ src/main/java/leetcode/everyday/AlgorithmUtil.java
@@ -3,11 +3,16 @@ package leetcode.everyday;
 public class AlgorithmUtil {
 
     public static void sysoutDobbleArray(int [][] array){
+        if(array == null){
+            return ;
+        }
+        System.out.println("-----------------------------");
         for(int i=0;i<array.length;i++){ //0,1,2
             for(int j=0;j<array[i].length;j++){ //每一个一维数组的长度
                 System.out.print(array[i][j]+"\t");
             }
             System.out.println();
         }
+        System.out.println("-----------------------------");
     }
 }
