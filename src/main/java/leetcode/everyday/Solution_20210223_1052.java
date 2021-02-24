8628e2dfe494d8f4ad9d40ed9b50fc5738f0b769
guozhizhong <guo_zhizhong@sina.com>
2021-02-25 00:13:17 +0800

    提交

--- src/main/java/leetcode/everyday/Solution_20210223_1052.java
+++ src/main/java/leetcode/everyday/Solution_20210223_1052.java
@@ -3,43 +3,24 @@ package leetcode.everyday;
 import org.springframework.util.Assert;
 
 /**
- *托普利茨矩阵
- * 给你一个 m x n 的矩阵 matrix 。如果这个矩阵是托普利茨矩阵，返回 true ；否则，返回 false 。
+ *1052. 爱生气的书店老板
+ * 今天，书店老板有一家店打算试营业 customers.length 分钟。每分钟都有一些顾客（customers[i]）会进入书店，所有这些顾客都会在那一分钟结束后离开。
  *
- * 如果矩阵上每一条由左上到右下的对角线上的元素都相同，那么这个矩阵是 托普利茨矩阵 。
+ * 在某些时候，书店老板会生气。 如果书店老板在第 i 分钟生气，那么 grumpy[i] = 1，否则 grumpy[i] = 0。 当书店老板生气时，那一分钟的顾客就会不满意，不生气则他们是满意的。
  *
+ * 书店老板知道一个秘密技巧，能抑制自己的情绪，可以让自己连续 X 分钟不生气，但却只能使用一次。
+ *
+ * 请你返回这一天营业下来，最多有多少客户能够感到满意的数量。
  *  
  *
- * 示例 1：
+ * 示例：
  *
- *
- * 输入：matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
- * 输出：true
+ * 输入：customers = [1,0,1,2,1,1,7,5], grumpy = [0,1,0,1,0,1,0,1], X = 3
+ * 输出：16
  * 解释：
- * 在上述矩阵中, 其对角线为:
- * "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]"。
- * 各条对角线上的所有元素均相同, 因此答案是 True 。
- * 示例 2：
- *
- *
- * 输入：matrix = [[1,2],[2,2]]
- * 输出：false
- * 解释：
- * 对角线 "[1, 2]" 上的元素不同。
+ * 书店老板在最后 3 分钟保持冷静。
+ * 感到满意的最大客户数量 = 1 + 1 + 1 + 1 + 7 + 5 = 16.
  *  
- *
- * 提示：
- *
- * m == matrix.length
- * n == matrix[i].length
- * 1 <= m, n <= 20
- * 0 <= matrix[i][j] <= 99
- *  
- *
- * 进阶：
- *
- * 如果矩阵存储在磁盘上，并且内存有限，以至于一次最多只能将矩阵的一行加载到内存中，该怎么办？
- * 如果矩阵太大，以至于一次只能将不完整的一行加载到内存中，该怎么办？
  *
  */
 
@@ -47,31 +28,46 @@ import org.springframework.util.Assert;
 public class Solution_20210223_1052 {
 
     /**
-     * 思路 1.只需判断当前行的元素与上一行的前一个元素是否相同 即可
-     *      1	2	3	4
-     *      5	1	2	3
-     *      9	5	1	2
-     *      时间复杂度：O(mn)O(mn)，其中 mm 为矩阵的行数，nn 为矩阵的列数。矩阵中每个元素至多被访问两次。
-     *      空间复杂度：O(1)O(1)，我们只需要常数的空间保存若干变量。
-     *
+     * 思路
+     * 1.先将原本就满意的客户加入答案,同时将对应的 customers[i] 变为 0。
+     * 2.之后的问题转化为：在 customers 中找到连续一段长度为 x 的子数组，使得其总和最大。
+     *  这部分就是我们应用技巧所得到的客户。(利用滑动窗口)
+     * @param customers
+     * @param grumpy
+     * @param X
+     * @return
      */
-    public static  boolean isToeplitzMatrix(int[][] matrix) {
-        int m=matrix.length,n=matrix[0].length;
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                if(matrix[i][j]!=matrix[i-1][j-1]){
-                    return false;
-                }
+    public static  int maxSatisfied(int[] customers, int[] grumpy, int X) {
+        int ans=0;
+        for(int i=0;i<customers.length;i++){
+            if(grumpy[i]==0){
+                ans=ans+customers[i];
+                customers[i]=0;
             }
         }
-            return true;
+        System.out.println("原本满意的人共有"+ans+"人");
+
+        int max=0,curMax=0;
+        for(int i=0,j=0;i<customers.length;i++){
+            curMax+=customers[i];
+            if(i-j+1>X){
+                curMax=curMax-customers[j];
+                j++;
+            }
+            max=Math.max(max,curMax);
+        }
+        ans=ans+max;
+        return ans;
+
     }
 
     public static void main(String[] args) {
-        int [][] a1 = new int[][]{{1,2,3,4},{5,1,2,3},{9,5,1,2}};
+        int [] customers  = new int[]{1,0,1,2,1,1,7,5};
+        int [] grumpy  = new int[]{0,1,0,1,0,1,0,1};
+        int X=3;
+
         System.out.println("-------------开始执行-------------");
-        AlgorithmUtil.sysoutDobbleArray(a1);
-        Assert.isTrue(isToeplitzMatrix(a1)==true,"程序异常");
+        Assert.isTrue(maxSatisfied(customers,grumpy,X)==16,"程序异常");
         System.out.println("-------------运行通过-------------");
 
     }
