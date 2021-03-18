cbae8ecd49cf973f3d3d0fb7056a9f56d4b2a00a
guozhizhong <guo_zhizhong@sina.com>
2021-03-18 23:27:32 +0800

    提交

--- src/main/java/leetcode/everyday/Solution_20210318_92.java
+++ src/main/java/leetcode/everyday/Solution_20210318_92.java
@@ -74,6 +74,58 @@ public class Solution_20210318_92 {
         return dummyHead.next;
     }
 
+    /**
+     * 头插法
+     * 思路：
+     *  先将 curr 的下一个节点记录为 next；
+     * 执行操作 ①：把 curr 的下一个节点指向 next 的下一个节点；
+     * 执行操作 ②：把 next 的下一个节点指向 pre 的下一个节点；
+     * 执行操作 ③：把 pre 的下一个节点指向 next。
+     * 如 链表为 1->2->3->4->5   left 为2   right为4   即翻转2,3,4
+     *    pre 为 1  cur为 2  next为 3
+     *
+     * @param head
+     * @param left
+     * @param right
+     * @return
+     */
+    public static ListNode reverseBetween2(ListNode head, int left, int right) {
+        //设置dummy 哑节点
+        ListNode dummy = new ListNode(-1);
+        dummy.next=head;
+        ListNode pre= dummy;
+
+        //找到要开始翻转节点
+        for(int i=0;i<left-1;i++){
+            pre=pre.next;
+        }
+
+        //第一种写法  先定义好next  此时在循环里要将next赋值
+//        ListNode cur=pre.next;
+//        ListNode next =cur.next;
+//        //left 到 right 间进行翻转
+//        for(int i=0;i<right-left;i++){
+//            cur.next=next.next;
+//            next.next=pre.next;
+//            pre.next=next;
+//            next=cur.next;
+//        }
+        //第二种写法,每次循环的时候获取next值
+        ListNode cur=pre.next;
+        for(int i=0;i<right-left;i++){
+            ListNode next =cur.next;
+            cur.next=next.next;
+            next.next=pre.next;
+            pre.next=next;
+        }
+
+        return dummy.next;
+
+
+    }
+
+
+
     public static void main(String[] args) {
         System.out.println("-------------开始执行-------------");
         //Assert.isTrue(numDistinct("rabbbit","rabbit")==3,"程序异常");
