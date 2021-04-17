9b40488e56f15c9569d14a2b1914783a72b4e7c8
guozhizhong <guo_zhizhong@sina.com>
2021-04-17 23:20:46 +0800

    提交

--- src/main/java/leetcode/everyday/april/Solution_20210417_220.java
+++ src/main/java/leetcode/everyday/april/Solution_20210417_220.java
@@ -2,6 +2,8 @@ package leetcode.everyday.april;
 
 import org.springframework.util.Assert;
 
+import java.util.HashMap;
+
 /**
  *q 220 存在重复元素 III
  *
@@ -29,17 +31,20 @@ import org.springframework.util.Assert;
  * 输出：false
  *  
  *
- * 提示：
- *
- * 0 <= nums.length <= 2 * 104
- * -231 <= nums[i] <= 231 - 1
- * 0 <= k <= 104
- * 0 <= t <= 231 - 1
  *
  */
 public class Solution_20210417_220 {
 
 
+    private static long w;
+
+    /**
+     * 暴力 超时
+     * @param nums
+     * @param k
+     * @param t
+     * @return
+     */
     public  static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
         for(int i=0;i<nums.length;i++){
             for(int j=0;j<i;j++){
@@ -52,7 +57,46 @@ public class Solution_20210417_220 {
     }
 
 
-    public static void main(String[] args) {
+    /**
+     * 桶排序
+     * @param nums
+     * @param k
+     * @param t
+     * @return
+     */
+    public  static boolean containsNearbyAlmostDuplicate2(int[] nums, int k, int t) {
+        /*
+            k --- index     1
+            t ---- value    2147483647
+        */
+        if(nums.length == 0 || t < 0)        return false;
+        //long ---id    long value
+        HashMap<Long, Long> map = new HashMap<>();
+        w = (long)t + 1;
+
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(i - k - 1 >= 0 && map.containsKey(getID(nums[i - k - 1])))
+                map.remove(getID(nums[i - k - 1]));
+            if(map.containsKey(getID(nums[i])))
+                return true;
+            if(map.containsKey(getID(nums[i]) - 1)
+                    && Math.abs(map.get(getID(nums[i]) - 1) - (long)nums[i]) <= t)
+                return true;
+            if(map.containsKey(getID(nums[i]) + 1)
+                    && Math.abs(map.get(getID(nums[i]) + 1) - (long)nums[i]) <= t)
+                return true;
+            map.put(getID(nums[i]), (long)nums[i]);
+        }
+        return false;
+    }
+
+    private static long getID(long num)
+    {
+        if(num >= 0)    return (long) num / w;
+        else            return (long) (num - 1) / w - 1;
+    }
+        public static void main(String[] args) {
         System.out.println("-------------开始执行-------------");
         //Assert.isTrue(containsNearbyAlmostDuplicate(new int[]{1,2,3,1},3,0)==true,"程序异常");
         //Assert.isTrue(containsNearbyAlmostDuplicate(new int[]{1,0,1,1},1,2)==true,"程序异常");
