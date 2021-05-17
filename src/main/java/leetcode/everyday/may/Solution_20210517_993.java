777af245f65bfa9ccbb66782f2c909d364abd35a
guozhizhong <guo_zhizhong@sina.com>
2021-05-17 10:09:35 +0800

    提交

---
+++ src/main/java/leetcode/everyday/may/Solution_20210517_993.java
@@ -0,0 +1,71 @@
+package leetcode.everyday.may;
+
+import leetcode.everyday.TreeNode;
+import org.springframework.util.Assert;
+
+/**
+ *q 993 二叉树的堂兄弟节点
+ *
+ * 在二叉树中，根节点位于深度 0 处，每个深度为 k 的节点的子节点位于深度 k+1 处。
+ *
+ * 如果二叉树的两个节点深度相同，但 父节点不同 ，则它们是一对堂兄弟节点。
+ *
+ * 我们给出了具有唯一值的二叉树的根节点 root ，以及树中两个不同节点的值 x 和 y 。
+ *
+ * 只有与值 x 和 y 对应的节点是堂兄弟节点时，才返回 true 。否则，返回 false。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,2,3,4], x = 4, y = 3
+ * 输出：false
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1,2,3,null,4,null,5], x = 5, y = 4
+ * 输出：true
+ * 示例 3：
+ *
+ *
+ *
+ * 输入：root = [1,2,3,null,4], x = 2, y = 3
+ * 输出：false
+ *  
+ *
+ * 提示：
+ *
+ * 二叉树的节点数介于 2 到 100 之间。
+ * 每个节点的值都是唯一的、范围为 1 到 100 的整数。
+ *
+ */
+public class Solution_20210517_993 {
+
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+        int[] xi = dfs(root, null, 0, x);
+        int[] yi = dfs(root, null, 0, y);
+        return xi[1] == yi[1] && xi[0] != yi[0];
+
+    }
+
+    int[] dfs(TreeNode root, TreeNode fa, int depth, int t) {
+        // 使用 -1 代表为搜索不到 t
+        if (root == null) return new int[]{-1, -1};
+        if (root.val == t) {
+            return new int[]{fa != null ? fa.val : 1, depth}; // 使用 1 代表搜索值 t 为 root
+        }
+        int[] l = dfs(root.left, root, depth + 1, t);
+        if (l[0] != -1) return l;
+        return dfs(root.right, root, depth + 1, t);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("-------------开始执行-------------");
+        //Assert.isTrue(findMaximumXOR(new int[]{3,10,5,25,2,8})==28,"程序异常");
+        //xorQueries(new int[]{1,3,4,8},new int [][]{{0,1},{1,2},{0,3},{3,3}});
+        System.out.println("-------------运行通过-------------");
+    }
+}
