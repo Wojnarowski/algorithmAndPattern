5cd26669bc69bef34a76143fa1354a958b01a352
guozhizhong <guo_zhizhong@sina.com>
2021-06-17 09:57:25 +0800

    提交

--- src/main/java/leetcode/github/lru/LRUCache.java
+++ src/main/java/leetcode/github/lru/LRUCache.java
@@ -42,6 +42,16 @@ public class LRUCache {
         int val;
         Node pre;
         Node next;
+
+//        @Override
+//        public String toString() {
+//            return "Node{" +
+//                    "key=" + key +
+//                    ", val=" + val +
+//                    ", pre=" + pre +
+//                    ", next=" + next +
+//                    '}';
+//        }
     }
     //模拟缓存容量
     private int capacity;
@@ -70,7 +80,27 @@ public class LRUCache {
     }
 
     public void put (int key,int value){
+        //看看节点是否已经存在
+        Node node = map.get(key);
+        //节点不存在
+        if(node==null){
+            //先创建节点，然后看节点是否满了，如果满了删除最后一个节点；然后将节点放到链表头部，增加一个映射关系
+            //否则直接覆盖，然后移动到链表头部
+            node = new Node();
+            node.key=key;
+            node.val=value;
 
+            if(map.size()==capacity){
+                removeLast();
+            }
+            addToHead(node);
+            map.put(key,node);
+        }
+        //节点存在直接放到前面
+        else{
+            node.val=value;
+            moveToHead(node);
+        }
     }
 
     private void moveToHead(Node node) {
@@ -95,14 +125,59 @@ public class LRUCache {
         }
 
         //把当前节点作为头结点
-
+        node.pre= first.pre;//也可以写成node.pre=first.pre;
+        node.next=first;
+        first.pre=node;
+        first=node;
     }
 
     private void addToHead(Node node) {
-
+        if(map.isEmpty()){
+            first=node;
+            last=node;
+        }
+        else{
+            //把新节点作为头节点
+            //node.pre=first.pre;
+            node.next=first;
+            first.pre=node;
+            first=node;
+        }
     }
 
     private void removeLast() {
+        map.remove(last.key);
+        Node preNode = last.pre;
+        //修改last指针位置
+        if(preNode != null){
+            preNode.next=null;
+            last=preNode;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(this.first.val);
+    }
+
+    public static void main(String[] args) {
+        LRUCache cache = new LRUCache(5);
+        cache.put(1,1);
+        System.out.println(cache);
+        cache.put(2,2);
+        System.out.println(cache);
+        cache.put(3,3);
+        System.out.println(cache);
+        cache.put(4,4);
+        System.out.println(cache);
+        cache.put(5,5);
+        System.out.println(cache);
+        cache.put(6,6);
+        System.out.println(cache);
+        cache.get(3);
+        System.out.println("调用get");
+        System.out.println(cache);
+
 
     }
 
