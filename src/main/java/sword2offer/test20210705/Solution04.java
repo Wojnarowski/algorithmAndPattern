79ab259b9221132e845b89d8a9a63a3e97452e40
guozhizhong <guo_zhizhong@sina.com>
2021-07-05 16:02:26 +0800

    提交

--- src/main/java/sword2offer/test20210705/Solution04.java
+++ src/main/java/sword2offer/test20210705/Solution04.java
@@ -63,25 +63,8 @@ package sword2offer.test20210705;
  */
 public class Solution04 {
 
-    /**
-     * 动态规划最快
-     * @param n
-     * @return
-     */
-    public int fib(int n) {
-        if(n==0){
-            return 0;
-        }
-        int [] dp= new int[n+1];
-        dp[0] = 0;
-        dp[1] = 1;
-
-        for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
-            dp[i] %= 1000000007;
-        }
-        return dp[n];
-
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        return true;
     }
 
 }
