f5c082c9ad37fb3a8fd3241aff48f63c96224833
guozhizhong <guo_zhizhong@sina.com>
2021-09-10 15:36:05 +0800

    提交

--- src/main/java/leetcode/everyday/september/Solution_20210910_1894.java
+++ src/main/java/leetcode/everyday/september/Solution_20210910_1894.java
@@ -55,8 +55,18 @@ import java.util.List;
 public class Solution_20210910_1894 {
 
     public int chalkReplacer(int[] chalk, int k) {
+        int n = chalk.length;
+        long[] sum = new long[n + 1];
+        for (int i = 1; i <= n; i++) sum[i] = sum[i - 1] + chalk[i - 1];
+        k = (int)(k % sum[n]);
+        int l = 1, r = n;
+        while (l < r) {
+            int mid = l + r + 1 >> 1;
+            if (sum[mid] <= k) l = mid;
+            else r = mid - 1;
+        }
+        return sum[r] <= k ? r : r - 1;
 
-        return 0;
     }
 
 
