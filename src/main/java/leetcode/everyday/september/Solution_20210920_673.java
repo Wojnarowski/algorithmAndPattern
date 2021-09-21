0df8ca516e70fc8f94aea1caef85cc3da6515002
guozhizhong <guo_zhizhong@sina.com>
2021-09-21 15:21:40 +0800

    提交

--- src/main/java/leetcode/everyday/september/Solution_20210920_673.java
+++ src/main/java/leetcode/everyday/september/Solution_20210920_673.java
@@ -30,20 +30,28 @@ public class Solution_20210920_673 {
 
     public int findNumberOfLIS(int[] nums) {
         int n = nums.length;
-        int maxLen = 1;
-
-        int[] dp = new int[n];
-        dp[0] = 1;
-        for (int i = 1; i < n; i++) {
+        int[] f = new int[n], g = new int[n];
+        int max = 1;
+        for (int i = 0; i < n; i++) {
+            f[i] = g[i] = 1;
             for (int j = 0; j < i; j++) {
                 if (nums[j] < nums[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    if (f[i] < f[j] + 1) {
+                        f[i] = f[j] + 1;
+                        g[i] = g[j];
+                    } else if (f[i] == f[j] + 1) {
+                        g[i] += g[j];
+                    }
                 }
             }
-            maxLen = Math.max(dp[i],maxLen);
+            max = Math.max(max, f[i]);
         }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (f[i] == max) ans += g[i];
+        }
+        return ans;
 
-        return maxLen;
 
     }
 
