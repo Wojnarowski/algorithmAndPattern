0191a0589a7040a3a308f3ef221bc9f72a42af1a
guozhizhong <guo_zhizhong@sina.com>
2021-10-13 09:16:44 +0800

    提交

---
+++ src/main/java/leetcode/everyday/october/Solution_20211013_412.java
@@ -0,0 +1,63 @@
+package leetcode.everyday.october;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *q 412 Fizz Buzz
+ *
+ * 写一个程序，输出从 1 到 n 数字的字符串表示。
+ *
+ * 1. 如果 n 是3的倍数，输出“Fizz”；
+ *
+ * 2. 如果 n 是5的倍数，输出“Buzz”；
+ *
+ * 3.如果 n 同时是3和5的倍数，输出 “FizzBuzz”。
+ *
+ * 示例：
+ *
+ * n = 15,
+ *
+ * 返回:
+ * [
+ *     "1",
+ *     "2",
+ *     "Fizz",
+ *     "4",
+ *     "Buzz",
+ *     "Fizz",
+ *     "7",
+ *     "8",
+ *     "Fizz",
+ *     "Buzz",
+ *     "11",
+ *     "Fizz",
+ *     "13",
+ *     "14",
+ *     "FizzBuzz"
+ * ]
+ *
+ *
+ */
+public class Solution_20211013_412 {
+
+    public List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            String cur = "";
+            if (i % 3 == 0) cur += "Fizz";
+            if (i % 5 == 0) cur += "Buzz";
+            if (cur.length() == 0) cur = i + "";
+            ans.add(cur);
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("-------------开始执行-------------");
+        int [] array= new int[]{1,2,3,1};
+        //Assert.isTrue(arrangeCoins(5)==2,"程序异常");
+        System.out.println("-------------运行通过-------------");
+    }
+}
