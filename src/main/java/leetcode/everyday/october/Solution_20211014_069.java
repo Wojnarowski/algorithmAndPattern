19936e243800052b37e6835638176a9581542b92
guozhizhong <guo_zhizhong@sina.com>
2021-10-14 18:16:01 +0800

    提交

---
+++ src/main/java/leetcode/everyday/october/Solution_20211014_069.java
@@ -0,0 +1,74 @@
+package leetcode.everyday.october;
+
+/**
+ *q 412 山峰数组的顶部
+ *
+ * 符合下列属性的数组 arr 称为 山峰数组（山脉数组） ：
+ *
+ * arr.length >= 3
+ * 存在 i（0 < i < arr.length - 1）使得：
+ * arr[0] < arr[1] < ... arr[i-1] < arr[i]
+ * arr[i] > arr[i+1] > ... > arr[arr.length - 1]
+ * 给定由整数组成的山峰数组 arr ，返回任何满足 arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1] 的下标 i ，即山峰顶部。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：arr = [0,1,0]
+ * 输出：1
+ * 示例 2：
+ *
+ * 输入：arr = [1,3,5,4,2]
+ * 输出：2
+ * 示例 3：
+ *
+ * 输入：arr = [0,10,5,2]
+ * 输出：1
+ * 示例 4：
+ *
+ * 输入：arr = [3,4,5,1]
+ * 输出：2
+ * 示例 5：
+ *
+ * 输入：arr = [24,69,100,99,79,78,67,36,26,19]
+ * 输出：2
+ *  
+ *
+ * 提示：
+ *
+ * 3 <= arr.length <= 104
+ * 0 <= arr[i] <= 106
+ * 题目数据保证 arr 是一个山脉数组
+ *
+ *
+ */
+public class Solution_20211014_069 {
+
+
+    /**
+     * 二分
+     *
+     * @param arr
+     * @return
+     */
+    public int peakIndexInMountainArray(int[] arr) {
+
+        int n=arr.length;
+        int l=1,r=n-1;
+        while(l<r){
+            int mid = l + r + 1 >> 1;
+            if (arr[mid - 1] < arr[mid]) l = mid;
+            else r = mid - 1;
+        }
+        return r;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("-------------开始执行-------------");
+        int [] array= new int[]{1,2,3,1};
+        //Assert.isTrue(arrangeCoins(5)==2,"程序异常");
+        System.out.println("-------------运行通过-------------");
+    }
+}
