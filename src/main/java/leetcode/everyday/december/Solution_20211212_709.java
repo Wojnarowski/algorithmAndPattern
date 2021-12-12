20f5f98b785effb6c90799ce1c4aa7b6c0772083
guozhizhong <guo_zhizhong@sina.com>
2021-12-12 22:46:03 +0800

    提交

---
+++ src/main/java/leetcode/everyday/december/Solution_20211212_709.java
@@ -0,0 +1,56 @@
+package leetcode.everyday.december;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *q 709  转换成小写字母
+ *
+ * 给你一个字符串 s ，将该字符串中的大写字母转换成相同的小写字母，返回新的字符串。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：s = "Hello"
+ * 输出："hello"
+ * 示例 2：
+ *
+ * 输入：s = "here"
+ * 输出："here"
+ * 示例 3：
+ *
+ * 输入：s = "LOVELY"
+ * 输出："lovely"
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= s.length <= 100
+ * s 由 ASCII 字符集中的可打印字符组成
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/to-lower-case
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *  
+ */
+public class Solution_20211212_709 {
+
+
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println("-------------开始执行-------------");
+        int [] array= new int[]{1,2,3,1};
+        //Assert.isTrue(maxPower("leet")==2,"程序异常");
+        System.out.println("-------------运行通过-------------");
+    }
+
+
+}
