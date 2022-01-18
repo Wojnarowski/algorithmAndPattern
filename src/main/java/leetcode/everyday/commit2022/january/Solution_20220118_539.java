e0c6e985a0d0db16687aeab176fdd87410cdb362
guozhizhong <guo_zhizhong@sina.com>
2022-01-18 08:37:47 +0800

    提交

---
+++ src/main/java/leetcode/everyday/commit2022/january/Solution_20220118_539.java
@@ -0,0 +1,60 @@
+package leetcode.everyday.commit2022.january;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 539  最小时间差
+ *
+ * 给定一个 24 小时制（小时:分钟 "HH:MM"）的时间列表，找出列表中任意两个时间的最小时间差并以分钟数表示。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：timePoints = ["23:59","00:00"]
+ * 输出：1
+ * 示例 2：
+ *
+ * 输入：timePoints = ["00:00","23:59","00:00"]
+ * 输出：0
+ *  
+ *
+ * 提示：
+ *
+ * 2 <= timePoints <= 2 * 104
+ * timePoints[i] 格式为 "HH:MM"
+ * 通过次数19,114提交次数30,738
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-time-difference
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ *
+ *
+ */
+public class Solution_20220118_539 {
+
+    public int findMinDifference(List<String> timePoints) {
+        int n = timePoints.size() * 2;
+        int[] nums = new int[n];
+        for (int i = 0, idx = 0; i < n / 2; i++, idx += 2) {
+            String[] ss = timePoints.get(i).split(":");
+            int h = Integer.parseInt(ss[0]), m = Integer.parseInt(ss[1]);
+            nums[idx] = h * 60 + m;
+            nums[idx + 1] = nums[idx] + 1440;
+        }
+        Arrays.sort(nums);
+        int ans = nums[1] - nums[0];
+        for (int i = 0; i < n - 1; i++) ans = Math.min(ans, nums[i + 1] - nums[i]);
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("-------------开始执行-------------");
+        //Assert.isTrue(clumsy(43261596)==964176192,"程序异常");
+        System.out.println("-------------运行通过-------------");
+
+    }
+}
