ebc925f86b301058b9ccd555cbc96c0bc919836e
guozhizhong <guo_zhizhong@sina.com>
2022-01-29 09:58:35 +0800

    提交

---
+++ src/main/java/leetcode/everyday/commit2022/january/Solution_20220129_1765.java
@@ -0,0 +1,97 @@
+package leetcode.everyday.commit2022.january;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+/**
+ * 1765  地图中的最高点
+ *
+ * 给你一个大小为 m x n 的整数矩阵 isWater ，它代表了一个由 陆地 和 水域 单元格组成的地图。
+ *
+ * 如果 isWater[i][j] == 0 ，格子 (i, j) 是一个 陆地 格子。
+ * 如果 isWater[i][j] == 1 ，格子 (i, j) 是一个 水域 格子。
+ * 你需要按照如下规则给每个单元格安排高度：
+ *
+ * 每个格子的高度都必须是非负的。
+ * 如果一个格子是是 水域 ，那么它的高度必须为 0 。
+ * 任意相邻的格子高度差 至多 为 1 。当两个格子在正东、南、西、北方向上相互紧挨着，就称它们为相邻的格子。（也就是说它们有一条公共边）
+ * 找到一种安排高度的方案，使得矩阵中的最高高度值 最大 。
+ *
+ * 请你返回一个大小为 m x n 的整数矩阵 height ，其中 height[i][j] 是格子 (i, j) 的高度。如果有多种解法，请返回 任意一个 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：isWater = [[0,1],[0,0]]
+ * 输出：[[1,0],[2,1]]
+ * 解释：上图展示了给各个格子安排的高度。
+ * 蓝色格子是水域格，绿色格子是陆地格。
+ * 示例 2：
+ *
+ *
+ *
+ * 输入：isWater = [[0,0,1],[1,0,0],[0,0,0]]
+ * 输出：[[1,1,0],[0,1,1],[1,2,2]]
+ * 解释：所有安排方案中，最高可行高度为 2 。
+ * 任意安排方案中，只要最高高度为 2 且符合上述规则的，都为可行方案。
+ *  
+ *
+ * 提示：
+ *
+ * m == isWater.length
+ * n == isWater[i].length
+ * 1 <= m, n <= 1000
+ * isWater[i][j] 要么是 0 ，要么是 1 。
+ * 至少有 1 个水域格子。
+ * 通过次数6,640提交次数10,908
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/map-of-highest-peak
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ *
+ *
+ */
+public class Solution_20220129_1765 {
+    public int[][] highestPeak(int[][] isWater) {
+        int m = isWater.length, n = isWater[0].length;
+        int[][] ans = new int[m][n];
+        Deque<int[]> d = new ArrayDeque<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isWater[i][j] == 1) d.addLast(new int[]{i, j});
+                ans[i][j] = isWater[i][j] == 1 ? 0 : -1;
+            }
+        }
+        int[][] dirs = new int[][]{{1,0}, {-1,0}, {0,1}, {0,-1}};
+        int h = 1;
+        while (!d.isEmpty()) {
+            int size = d.size();
+            while (size-- > 0) {
+                int[] info = d.pollFirst();
+                int x = info[0], y = info[1];
+                for (int[] di : dirs) {
+                    int nx = x + di[0], ny = y + di[1];
+                    if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+                    if (ans[nx][ny] != -1) continue;
+                    ans[nx][ny] = h;
+                    d.addLast(new int[]{nx, ny});
+                }
+            }
+            h++;
+        }
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("-------------开始执行-------------");
+        //Assert.isTrue(clumsy(43261596)==964176192,"程序异常");
+        System.out.println("-------------运行通过-------------");
+
+    }
+}
