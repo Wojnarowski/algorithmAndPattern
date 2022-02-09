e6e20389d9d17efd419e806a115bd50d53440489
guozhizhong <guo_zhizhong@sina.com>
2022-02-09 14:25:38 +0800

    提交

---
+++ src/main/java/leetcode/everyday/commit2022/february/Solution_20220209_2006.java
@@ -0,0 +1,74 @@
+package leetcode.everyday.commit2022.february;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 2006  差的绝对值为 K 的数对数目
+ *
+ * 给你一个整数数组 nums 和一个整数 k ，请你返回数对 (i, j) 的数目，满足 i < j 且 |nums[i] - nums[j]| == k 。
+ *
+ * |x| 的值定义为：
+ *
+ * 如果 x >= 0 ，那么值为 x 。
+ * 如果 x < 0 ，那么值为 -x 。
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,2,2,1], k = 1
+ * 输出：4
+ * 解释：差的绝对值为 1 的数对为：
+ * - [1,2,2,1]
+ * - [1,2,2,1]
+ * - [1,2,2,1]
+ * - [1,2,2,1]
+ * 示例 2：
+ *
+ * 输入：nums = [1,3], k = 3
+ * 输出：0
+ * 解释：没有任何数对差的绝对值为 3 。
+ * 示例 3：
+ *
+ * 输入：nums = [3,2,1,5,4], k = 2
+ * 输出：3
+ * 解释：差的绝对值为 2 的数对为：
+ * - [3,2,1,5,4]
+ * - [3,2,1,5,4]
+ * - [3,2,1,5,4]
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 200
+ * 1 <= nums[i] <= 100
+ * 1 <= k <= 99
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/count-number-of-pairs-with-absolute-difference-k
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ *
+ *
+ */
+public class Solution_20220209_2006 {
+
+    public int countKDifference(int[] nums, int k) {
+        int n = nums.length, ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (Math.abs(nums[i] - nums[j]) == k) ans++;
+            }
+        }
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("-------------开始执行-------------");
+        //Assert.isTrue(reversePrefix("abcd",'z').equals("abcd"),"程序异常");
+        System.out.println("-------------运行通过-------------");
+    }
+}
