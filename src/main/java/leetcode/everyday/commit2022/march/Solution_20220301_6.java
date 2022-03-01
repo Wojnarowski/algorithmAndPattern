55acda09114032233c083a17566c608319eb2a1c
guozhizhong <guo_zhizhong@sina.com>
2022-03-01 09:20:21 +0800

    提交

---
+++ src/main/java/leetcode/everyday/commit2022/march/Solution_20220301_6.java
@@ -0,0 +1,70 @@
+package leetcode.everyday.commit2022.march;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *6. Z 字形变换
+ *
+ * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+ *
+ * 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
+ *
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
+ *
+ * 请你实现这个将字符串进行指定行数变换的函数：
+ *
+ * string convert(string s, int numRows);
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：s = "PAYPALISHIRING", numRows = 3
+ * 输出："PAHNAPLSIIGYIR"
+ * 示例 2：
+ * 输入：s = "PAYPALISHIRING", numRows = 4
+ * 输出："PINALSIGYAHRPI"
+ * 解释：
+ * P     I    N
+ * A   L S  I G
+ * Y A   H R
+ * P     I
+ * 示例 3：
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/zigzag-conversion
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ *
+ */
+public class Solution_20220301_6 {
+
+
+    public String convert(String s, int numRows) {
+        if(numRows < 2) return s;
+        List<StringBuilder> rows = new ArrayList<StringBuilder>();
+        for(int i = 0; i < numRows; i++) rows.add(new StringBuilder());
+        int i = 0, flag = -1;
+        for(char c : s.toCharArray()) {
+            rows.get(i).append(c);
+            if(i == 0 || i == numRows -1) flag = - flag;
+            i += flag;
+        }
+        StringBuilder res = new StringBuilder();
+        for(StringBuilder row : rows) res.append(row);
+        return res.toString();
+
+    }
+
+    public static void main(String[] args) {
+
+
+        System.out.println("-------------开始执行-------------");
+        //Assert.isTrue(clumsy(43261596)==964176192,"程序异常");
+        System.out.println("-------------运行通过-------------");
+
+    }
+}
