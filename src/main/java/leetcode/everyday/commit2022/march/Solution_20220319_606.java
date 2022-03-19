9fa384809dbd7383ddd4f04541deb634d7d81195
guozhizhong <guo_zhizhong@sina.com>
2022-03-19 22:39:27 +0800

    提交

---
+++ src/main/java/leetcode/everyday/commit2022/march/Solution_20220319_606.java
@@ -0,0 +1,85 @@
+package leetcode.everyday.commit2022.march;
+
+/**
+ * 606  根据二叉树创建字符串
+ *
+ * 你需要采用前序遍历的方式，将一个二叉树转换成一个由括号和整数组成的字符串。
+ *
+ * 空节点则用一对空括号 "()" 表示。而且你需要省略所有不影响字符串与原始二叉树之间的一对一映射关系的空括号对。
+ *
+ * 示例 1:
+ *
+ * 输入: 二叉树: [1,2,3,4]
+ *        1
+ *      /   \
+ *     2     3
+ *    /
+ *   4
+ *
+ * 输出: "1(2(4))(3)"
+ *
+ * 解释: 原本将是“1(2(4)())(3())”，
+ * 在你省略所有不必要的空括号对之后，
+ * 它将是“1(2(4))(3)”。
+ * 示例 2:
+ *
+ * 输入: 二叉树: [1,2,3,null,4]
+ *        1
+ *      /   \
+ *     2     3
+ *      \
+ *       4
+ *
+ * 输出: "1(2()(4))(3)"
+ *
+ * 解释: 和第一个示例相似，
+ * 除了我们不能省略第一个对括号来中断输入和输出之间的一对一映射关系。
+ * 通过次数49,860提交次数80,679
+ * 请问您在哪类招聘中遇到此题？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/construct-string-from-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *  
+ *
+ *
+ */
+public class Solution_20220319_606 {
+
+
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+    StringBuilder sb = new StringBuilder();
+    public String tree2str(TreeNode root) {
+        dfs(root);
+        return sb.substring(1, sb.length() - 1);
+    }
+
+    private void dfs(TreeNode root) {
+        sb.append("(");
+        sb.append(root.val);
+        if (root.left != null) dfs(root.left);
+        else if (root.right != null) sb.append("()");
+        if (root.right != null) dfs(root.right);
+        sb.append(")");
+    }
+
+    public static void main(String[] args) {
+
+
+        System.out.println("-------------开始执行-------------");
+        //Assert.isTrue(clumsy(43261596)==964176192,"程序异常");
+        System.out.println("-------------运行通过-------------");
+
+    }
+}
