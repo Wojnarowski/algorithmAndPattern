ae7792a0f3aef8957526ba1805e091b4a99525af
guozhizhong <guo_zhizhong@sina.com>
2022-04-14 09:10:55 +0800

    提交

---
+++ src/main/java/leetcode/everyday/commit2022/april/Solution_20220414_1672.java
@@ -0,0 +1,61 @@
+package leetcode.everyday.commit2022.april;
+
+import java.util.*;
+
+/**
+ * 1672   最富有客户的资产总量
+ *
+ * 给你一个 m x n 的整数网格 accounts ，其中 accounts[i][j] 是第 i​​​​​​​​​​​​ 位客户在第 j 家银行托管的资产数量。返回最富有客户所拥有的 资产总量 。
+ *
+ * 客户的 资产总量 就是他们在各家银行托管的资产数量之和。最富有客户就是 资产总量 最大的客户。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：accounts = [[1,2,3],[3,2,1]]
+ * 输出：6
+ * 解释：
+ * 第 1 位客户的资产总量 = 1 + 2 + 3 = 6
+ * 第 2 位客户的资产总量 = 3 + 2 + 1 = 6
+ * 两位客户都是最富有的，资产总量都是 6 ，所以返回 6 。
+ * 示例 2：
+ *
+ * 输入：accounts = [[1,5],[7,3],[3,5]]
+ * 输出：10
+ * 解释：
+ * 第 1 位客户的资产总量 = 6
+ * 第 2 位客户的资产总量 = 10
+ * 第 3 位客户的资产总量 = 8
+ * 第 2 位客户是最富有的，资产总量是 10
+ * 示例 3：
+ *
+ * 输入：accounts = [[2,8,7],[7,1,3],[1,9,5]]
+ * 输出：17
+ *
+ *
+ *
+ *
+ */
+public class Solution_20220414_1672 {
+
+
+    public int maximumWealth(int[][] accounts) {
+        int m = accounts.length, n = accounts[0].length, ans = 0;
+        for(int i=0;i<m;i++){
+            int cur = 0;
+            for(int j=0;j<n;j++ ){
+                cur+=accounts[i][j];
+            }
+            ans=Math.max(ans,cur);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("-------------开始执行-------------");
+        //Assert.isTrue(clumsy(43261596)==964176192,"程序异常");
+        System.out.println("-------------运行通过-------------");
+
+    }
+}
