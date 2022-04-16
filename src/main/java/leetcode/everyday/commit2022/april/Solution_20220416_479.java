7e7a2268c9a68e8675dc48af3d6a041612568f54
guozhizhong <guo_zhizhong@sina.com>
2022-04-16 22:12:32 +0800

    提交

---
+++ src/main/java/leetcode/everyday/commit2022/april/Solution_20220416_479.java
@@ -0,0 +1,65 @@
+package leetcode.everyday.commit2022.april;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * 479  最大回文数乘积
+ *
+ * 给定一个整数 n ，返回 可表示为两个 n 位整数乘积的 最大回文整数 。因为答案可能非常大，所以返回它对 1337 取余 。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入：n = 2
+ * 输出：987
+ * 解释：99 x 91 = 9009, 9009 % 1337 = 987
+ * 示例 2:
+ *
+ * 输入： n = 1
+ * 输出： 9
+ *  
+ *
+ * 提示:
+ *
+ * 1 <= n <= 8
+ * 通过次数17,789提交次数28,958
+ * 请问您在哪类招聘中遇到此题？
+ *
+ *
+ *
+ */
+public class Solution_20220416_479 {
+
+    public int largestPalindrome(int n) {
+        if (n == 1) {
+            return 9;
+        }
+        int upper = (int) Math.pow(10, n) - 1;
+        int ans = 0;
+        for (int left = upper; ans == 0; --left) { // 枚举回文数的左半部分
+            long p = left;
+            for (int x = left; x > 0; x /= 10) {
+                p = p * 10 + x % 10; // 翻转左半部分到其自身末尾，构造回文数 p
+            }
+            for (long x = upper; x * x >= p; --x) {
+                if (p % x == 0) { // x 是 p 的因子
+                    ans = (int) (p % 1337);
+                    break;
+                }
+            }
+        }
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("-------------开始执行-------------");
+        //Assert.isTrue(clumsy(43261596)==964176192,"程序异常");
+        System.out.println("-------------运行通过-------------");
+
+    }
+}
