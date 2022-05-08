01412fc34d9078cb9eaf89764bf6f2e08887602d
guozhizhong <guo_zhizhong@sina.com>
2022-05-08 22:44:47 +0800

    提交

---
+++ src/main/java/leetcode/everyday/commit/Solution_20220508_442.java
@@ -0,0 +1,31 @@
+package leetcode.everyday.commit;
+
+import java.util.*;
+
+/**
+ *442 数组中重复的数据
+ *
+ */
+
+
+public class Solution_20220508_442 {
+
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int num : nums) {
+            if (nums[Math.abs(num) - 1] < 0) {
+                res.add(Math.abs(num));
+            } else {
+                nums[Math.abs(num) - 1] *= -1;
+            }
+        }
+        return res;
+
+    }
+
+
+    public static void main(String[] args) {
+        int [] a = new int[]{0,1,0};
+
+    }
+}
