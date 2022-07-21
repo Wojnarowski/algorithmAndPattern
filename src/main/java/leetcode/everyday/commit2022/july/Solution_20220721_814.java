02d416cb85fd54452e3c53823ca5460bdb168a6d
guozhizhong <guo_zhizhong@sina.com>
2022-07-21 09:08:42 +0800

    提交

---
+++ src/main/java/leetcode/everyday/commit2022/july/Solution_20220721_814.java
@@ -0,0 +1,67 @@
+package leetcode.everyday.commit2022.july;
+
+import leetcode.everyday.TreeNode;
+
+/**
+ * 1260  二维网格迁移
+ *
+ *
+ * 给你二叉树的根结点 root ，此外树的每个结点的值要么是 0 ，要么是 1 。
+ *
+ * 返回移除了所有不包含 1 的子树的原二叉树。
+ *
+ * 节点 node 的子树为 node 本身加上所有 node 的后代。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,null,0,0,1]
+ * 输出：[1,null,0,null,1]
+ * 解释：
+ * 只有红色节点满足条件“所有不包含 1 的子树”。 右图为返回的答案。
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1,0,1,0,0,0,1]
+ * 输出：[1,null,1,null,1]
+ * 示例 3：
+ *
+ *
+ * 输入：root = [1,1,0,1,1,0,1,0]
+ * 输出：[1,1,0,1,1,null,1]
+ *  
+ *
+ * 提示：
+ *
+ * 树中节点的数目在范围 [1, 200] 内
+ * Node.val 为 0 或 1
+ *
+ *
+ *
+ *
+ */
+
+
+public class Solution_20220721_814 {
+
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null){
+            return null;
+        }
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if (root.val == 0 && root.left == null && root.right == null ) {
+            return null;
+        } else {
+            return root;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int [] a = new int[]{0,1,0};
+
+    }
+}
