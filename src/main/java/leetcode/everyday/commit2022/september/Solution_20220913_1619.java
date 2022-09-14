0eb3b04d13bf55c248bf1f46f41c5c8a125de91b
guozhizhong <guo_zhizhong@sina.com>
2022-09-14 09:16:33 +0800

    提交

---
+++ src/main/java/leetcode/everyday/commit2022/september/Solution_20220913_1619.java
@@ -0,0 +1,20 @@
+package leetcode.everyday.commit2022.september;
+
+import java.util.Arrays;
+
+/**
+ * 1619     最大交换
+ *
+ *
+ *
+ */
+public class Solution_20220913_1619 {
+
+    public double trimMean(int[] arr) {
+        Arrays.sort(arr);
+        int n = arr.length, tot = 0;
+        for (int i = n / 20; i < n - n / 20; i++) tot += arr[i];
+        return tot * 1.0 / (n * 0.9);
+    }
+
+}
