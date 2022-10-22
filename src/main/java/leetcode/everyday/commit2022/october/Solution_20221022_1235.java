229a43771b997738a878d8330f75ba6f30e5f95a
guozhizhong <guo_zhizhong@sina.com>
2022-10-22 21:43:57 +0800

    提交

---
+++ src/main/java/leetcode/everyday/commit2022/october/Solution_20221022_1235.java
@@ -0,0 +1,30 @@
+package leetcode.everyday.commit2022.october;
+
+import java.util.*;
+
+/**
+ * 1235. 规划兼职工作
+ */
+public class Solution_20221022_1235 {
+
+    public int jobScheduling(int[] st, int[] et, int[] ps) {
+        int n = st.length;
+        List<int[]> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) list.add(new int[]{st[i], et[i], ps[i]});
+        Collections.sort(list, (a, b)->a[1] - b[1]);
+        int[] f = new int[n + 10];
+        for (int i = 1; i <= n; i++) {
+            int[] info = list.get(i - 1);
+            int a = info[0], b = info[1], c = info[2];
+            f[i] = Math.max(f[i - 1], c);
+            int l = 0, r = i - 1;
+            while (l < r) {
+                int mid = l + r + 1 >> 1;
+                if (list.get(mid)[1] <= a) l = mid;
+                else r = mid - 1;
+            }
+            if (list.get(r)[1] <= a) f[i] = Math.max(f[i], f[r + 1] + c);
+        }
+        return f[n];
+    }
+}
