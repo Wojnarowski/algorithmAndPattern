180698181a6a34d1e5b52a3435ec3d321575e76b
guozhizhong <guo_zhizhong@sina.com>
2022-10-24 09:10:39 +0800

    提交

---
+++ src/main/java/leetcode/everyday/commit2022/october/Solution_20221024_915.java
@@ -0,0 +1,19 @@
+package leetcode.everyday.commit2022.october;
+
+/**
+ * 915. 分割数组
+ */
+public class Solution_20221024_915 {
+
+    public int partitionDisjoint(int[] nums) {
+        int n = nums.length;
+        int[] min = new int[n + 10];
+        min[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) min[i] = Math.min(min[i + 1], nums[i]);
+        for (int i = 0, max = 0; i < n - 1; i++) {
+            max = Math.max(max, nums[i]);
+            if (max <= min[i + 1]) return i + 1;
+        }
+        return -1; // never
+    }
+}
