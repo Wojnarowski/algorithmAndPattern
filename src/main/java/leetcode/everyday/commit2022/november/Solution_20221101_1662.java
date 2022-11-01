b02f283beacd6345a5e80aab177fc2d5000b2891
guozhizhong <guo_zhizhong@sina.com>
2022-11-01 09:14:07 +0800

    提交

---
+++ src/main/java/leetcode/everyday/commit2022/november/Solution_20221101_1662.java
@@ -0,0 +1,12 @@
+package leetcode.everyday.commit2022.november;
+
+/**
+ * 769. 最多能完成排序的块
+ *
+ */
+public class Solution_20221101_1662 {
+
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        return String.join("", word1).equals(String.join("", word2));
+    }
+}
