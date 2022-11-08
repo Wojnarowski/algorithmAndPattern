3f747b867de3376450b1356d8a813b7651631b4a
guozhizhong <guo_zhizhong@sina.com>
2022-11-08 09:39:13 +0800

    提交

---
+++ src/main/java/leetcode/everyday/commit2022/november/Solution_20221108_1684.java
@@ -0,0 +1,33 @@
+package leetcode.everyday.commit2022.november;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 1684. 统计一致字符串的数目
+ *
+ */
+public class Solution_20221108_1684 {
+
+    public int countConsistentStrings(String allowed, String[] words) {
+        boolean[] s = new boolean[26];
+        for (char c : allowed.toCharArray()) {
+            s[c - 'a'] = true;
+        }
+        int ans = 0;
+        for (String w : words) {
+            if (check(w, s)) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+    private boolean check(String w, boolean[] s) {
+        for (int i = 0; i < w.length(); ++i) {
+            if (!s[w.charAt(i) - 'a']) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
