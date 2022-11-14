f4716fc83339a70e789312bb604b2b6952a761bb
guozhizhong <guo_zhizhong@sina.com>
2022-11-14 09:38:14 +0800

    提交

---
+++ src/main/java/leetcode/everyday/commit2022/november/Solution_20221114_805.java
@@ -0,0 +1,46 @@
+package leetcode.everyday.commit2022.november;
+
+import java.util.*;
+
+/**
+ * 805. 数组的均值分割
+ *
+ */
+public class Solution_20221114_805 {
+    public boolean splitArraySameAverage(int[] nums) {
+        int n = nums.length;
+        if (n == 1) {
+            return false;
+        }
+        int s = Arrays.stream(nums).sum();
+        for (int i = 0; i < n; ++i) {
+            nums[i] = nums[i] * n - s;
+        }
+        int m = n >> 1;
+        Set<Integer> vis = new HashSet<>();
+        for (int i = 1; i < 1 << m; ++i) {
+            int t = 0;
+            for (int j = 0; j < m; ++j) {
+                if (((i >> j) & 1) == 1) {
+                    t += nums[j];
+                }
+            }
+            if (t == 0) {
+                return true;
+            }
+            vis.add(t);
+        }
+        for (int i = 1; i < 1 << (n - m); ++i) {
+            int t = 0;
+            for (int j = 0; j < (n - m); ++j) {
+                if (((i >> j) & 1) == 1) {
+                    t += nums[m + j];
+                }
+            }
+            if (t == 0 || (i != (1 << (n - m)) - 1) && vis.contains(-t)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
