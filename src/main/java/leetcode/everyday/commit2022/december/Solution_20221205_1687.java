c3499ceeee88cf6916afdfb0d2d2c3c183bfe19c
guozhizhong <guo_zhizhong@sina.com>
2022-12-05 09:03:34 +0800

    提交

--- src/main/java/leetcode/everyday/commit2022/december/Solution_20221205_1687.java
+++ src/main/java/leetcode/everyday/commit2022/december/Solution_20221205_1687.java
@@ -1,6 +1,7 @@
 package leetcode.everyday.commit2022.december;
 
 import java.util.ArrayList;
+import java.util.*;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,26 +14,22 @@ public class Solution_20221205_1687 {
         int n = boxes.length;
         int[] dp = new int[n + 5];
         Arrays.fill(dp, 0x3f3f3f3f);
-        dp[0] = 0; //初始状态为0
+        dp[0] = 0;
+        Deque<int[]> q = new ArrayDeque<int[]>(); //双端队列
+        int dif = 0, wei = 0;
         for (int i = 1; i <= n; i++) {
-            int sum = 0;
-            for (int j = i; j >= 1 && j >= i - maxBoxes + 1; j--) {
-                sum += boxes[j - 1][1]; //累加箱子的种类之和
-                if (sum > maxWeight) break; //超过了最大重量
-                dp[i] = Math.min(dp[i], dp[j - 1] + cost(boxes, j, i));
-            }
+            int cur = dp[i - 1] + 2;//cur为每次滑动窗口增加的值即dp[i-1]+cost[i,i]
+            dif += i >= 2 && boxes[i - 1][0] != boxes[i - 2][0] ? 1 : 0;//dif为运输累加值，由于我们无法直接在队列中进行修改，那么可以考虑增加一个累加值
+            wei += boxes[i - 1][1]; //重量要加上当前箱子的重量
+            while (!q.isEmpty() && q.peekLast()[1] + dif >= cur) q.pollLast(); //构造一个单调递增的队列
+            q.add(new int[]{i, cur - dif, boxes[i - 1][1] - wei});
+            //判断左端队头是否在窗口外 并且重量不能超过最大重量
+            while (q.peekFirst()[0] <= i - maxBoxes || q.peekFirst()[2] + wei > maxWeight) q.pollFirst();
+            dp[i] = q.peekFirst()[1] + dif;
         }
         return dp[n];
+
     }
 
-    int cost(int[][] boxes, int l, int r) {
-        int ans = 2, port = boxes[l - 1][0]; //初始话为2,因为返回仓库算一次行程
-        while (++l <= r) {
-            if (boxes[l - 1][0] == port) continue; //只要相同，那么次数不会增加
-            ans++;  //码头不相同运输次数增加1
-            port = boxes[l - 1][0];
-        }
-        return ans;
-    }
 
 }
