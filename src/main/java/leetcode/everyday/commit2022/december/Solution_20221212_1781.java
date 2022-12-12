ca4e770f65a854b75481b5c15fcb1fda282d787a
guozhizhong <guo_zhizhong@sina.com>
2022-12-12 23:04:58 +0800

    提交

---
+++ src/main/java/leetcode/everyday/commit2022/december/Solution_20221212_1781.java
@@ -0,0 +1,28 @@
+package leetcode.everyday.commit2022.december;
+
+/**
+ * 1781. 所有子字符串美丽值之和
+ *
+ */
+public class Solution_20221212_1781 {
+
+    public int beautySum(String s) {
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int[] cnt = new int[26];
+            int maxFreq = 0;
+            for (int j = i; j < s.length(); j++) {
+                cnt[s.charAt(j) - 'a']++;
+                maxFreq = Math.max(maxFreq, cnt[s.charAt(j) - 'a']);
+                int minFreq = s.length();
+                for (int k = 0; k < 26; k++) {
+                    if (cnt[k] > 0) {
+                        minFreq = Math.min(minFreq, cnt[k]);
+                    }
+                }
+                res += maxFreq - minFreq;
+            }
+        }
+        return res;
+    }
+}
