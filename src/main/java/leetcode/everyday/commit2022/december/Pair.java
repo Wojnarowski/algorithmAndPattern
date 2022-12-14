1f078d7e6db0a796e4f24c5c4d454a22f0abdc38
guozhizhong <guo_zhizhong@sina.com>
2022-12-14 09:47:21 +0800

    提交

---
+++ src/main/java/leetcode/everyday/commit2022/december/Pair.java
@@ -0,0 +1,12 @@
+package leetcode.everyday.commit2022.december;
+
+public class Pair {
+    int a, b, l, index;
+
+    public Pair(int a, int b, int l, int index) {
+        this.a = a;
+        this.b = b;
+        this.l = l;
+        this.index = index;
+    }
+}
