c7db4eafa2d4c797e1d577a67b972ef4c997074a
guozhizhong <guo_zhizhong@sina.com>
2022-12-25 22:47:00 +0800

    提交

---
+++ src/main/java/leetcode/everyday/commit2022/december/Solution_20221225_1739.java
@@ -0,0 +1,25 @@
+package leetcode.everyday.commit2022.december;
+
+/**
+ * 1739. 放置盒子
+ *
+ */
+public class Solution_20221225_1739 {
+    public int minimumBoxes(int n) {
+        int s = 0, k = 1;
+        while (s + k * (k + 1) / 2 <= n) {
+            s += k * (k + 1) / 2;
+            ++k;
+        }
+        --k;
+        int ans = k * (k + 1) / 2;
+        k = 1;
+        while (s < n) {
+            ++ans;
+            s += k;
+            ++k;
+        }
+        return ans;
+    }
+}
+
