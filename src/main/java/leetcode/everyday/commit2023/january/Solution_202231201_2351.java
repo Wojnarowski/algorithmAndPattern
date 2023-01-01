77d57e44a73ef477a9dd85c87f191d90f4d49ce4
guozhizhong <guo_zhizhong@sina.com>
2023-01-01 21:41:55 +0800

    提交

---
+++ src/main/java/leetcode/everyday/commit2023/january/Solution_202231201_2351.java
@@ -0,0 +1,20 @@
+package leetcode.everyday.commit2023.january;
+
+import java.util.*;
+
+/**
+ * 2351. 第一个出现两次的字母
+ */
+public class Solution_202231201_2351 {
+
+    public char repeatedCharacter(String s) {
+        Set<Character> seen = new HashSet<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!seen.add(ch)) {
+                return ch;
+            }
+        }
+        return ' ';
+    }
+}
