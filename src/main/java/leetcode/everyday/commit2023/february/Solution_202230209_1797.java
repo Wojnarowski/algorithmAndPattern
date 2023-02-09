0fa011a11ac2d31a56f47e2d081013dd019b7581
guozhizhong <guo_zhizhong@sina.com>
2023-02-09 10:31:17 +0800

    提交

---
+++ src/main/java/leetcode/everyday/commit2023/february/Solution_202230209_1797.java
@@ -0,0 +1,27 @@
+package leetcode.everyday.commit2023.february;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 1233. 删除子文件夹
+ */
+public class Solution_202230209_1797 {
+
+    public Solution_202230209_1797(int timeToLive) {
+    }
+
+    public void generate(String tokenId, int currentTime) {
+
+    }
+
+    public void renew(String tokenId, int currentTime) {
+
+    }
+
+    public int countUnexpiredTokens(int currentTime) {
+        return 0;
+    }
+}
